2ef138c8f6aeea30244c6a83dbdb176225f5bbbb
ffiargus <rm.chow@outlook.com>
2019-03-22 22:01:42 -0400

    add speech recognition and marker

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() => runApp(MaterialApp(
     title: 'homeOTA',
@@ -18,6 +19,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Completer<GoogleMapController> _controller = Completer();
+
+  Set<Marker> markers = new Set.from([
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(43.66014526, -79.37840775),
+      icon: BitmapDescriptor.defaultMarker,
+      infoWindow: InfoWindow(
+        title: 'St. Vincent De Paul - Mary\'s Home',
+        snippet: '5 Star Rating',
+      )
+    )
+  ]);
 
   static const LatLng _center = const LatLng(43.646343, -79.383252);
 
@@ -46,6 +59,7 @@ class _HomeScreenState extends State<HomeScreen> {
           target: _center,
           zoom: 15.0,
         ),
+        markers: markers,
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.mic),
@@ -61,7 +75,53 @@ class Modal extends StatefulWidget {
 }
 
 class _ModalState extends State<Modal> {
+  SpeechRecognition _speech;
   bool _finishedRecording = false;
+  bool _speechRecognitionAvailable = false;
+  bool _isListening = false;
+  String transscription = '';
+
+  @override
+  void initState() {
+    super.initState();
+    activateSpeechRecognizer();
+  }
+
+  void start() => _speech
+    .listen(locale: 'en_US')
+    .then((result) => print('_MyAppState.start => result $result'));
+
+  void onSpeechAvailability(bool result) {
+    print('speech available!');
+    setState(() => _speechRecognitionAvailable = result);
+    start();
+  }
+
+  void activateSpeechRecognizer() {
+    print('_ModalState.activateSpeechRecognizer... ');
+    _speech = new SpeechRecognition();
+    _speech.setAvailabilityHandler(onSpeechAvailability);
+    // _speech.setCurrentLocaleHandler(onCurrentLocale);
+    _speech.setRecognitionStartedHandler(() => setState(() => _isListening = true));
+    _speech.setRecognitionResultHandler((String text) => setState(() => transscription = text));
+    _speech.setRecognitionCompleteHandler(() => setState(() => _isListening = false));
+    _speech
+      .activate()
+      .then((res) => setState(() {
+        print('speect activated! $res');
+        _speechRecognitionAvailable = res;
+        start();
+      }));
+  }
+
+  void onFinishedRecording() {
+    _speech.stop().then((result) {
+      setState(() {
+        _isListening = result;
+        _finishedRecording = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +134,7 @@ class _ModalState extends State<Modal> {
           closeModal: () => Navigator.pop(context),
         ) :
         RecordScreen(
-          onFinishRecording: () => setState(() => this._finishedRecording = true),
+          onFinishRecording: onFinishedRecording,
         )
     );
   }
